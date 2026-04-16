9e8b8a3e5b7637c0e852f53cff3b8cebf578bb8c
Angel Eduardo Terriquez Ochoa <aterriquez@ucol.mx>
2026-04-16 08:25:07 -0600

    mas cambios para subirlo a render

--- server.js
+++ server.js
@@ -67,6 +67,10 @@ app.delete('/vehiculos/:id', (req, res) => {
     res.json(nuevo_almacen.borrarVehiculo(id));
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000')
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('Server is running on port: ' + PORT);
 })
+
+// http://localhost:3000
